c93e0b3a525f7567b969881bed372935c818e04c
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2023-05-19 01:12:10 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.get("/", (req, res) => {
 });
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.beeiwwt.mongodb.net/?retryWrites=true&w=majority`;
-
+// DB_NAME = toyTown
+// DB_PASSWORD = RNCNztz4CkGJY0gJ
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,6 +29,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // create database
+    const toyTownDB = client.db("toyTownDB");
+    // create toy collection
+    const toyCollection = toyTownDB.collection("toys");
+
+    // post toys on db
+    app.post("/toys", async (req, res) => {
+      const toyDetails = req.body;
+      //   const result = await toyCollection.insertOne();
+      console.log(toyDetails);
+      res.send(toyDetails);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
