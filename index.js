e99aafd6b51eab65a963c4005d6732883cd1116b
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2023-05-19 10:29:18 +0600

    create some restful api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 4040;
 
@@ -29,17 +29,45 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    //---------------------------------------------------
     // create database
     const toyTownDB = client.db("toyTownDB");
     // create toy collection
     const toyCollection = toyTownDB.collection("toys");
+    //---------------------------------------------------
+    // create index
+    //---------------------------------------------------
+
+    //---------------------------------------------------
 
     // post toys on db
     app.post("/toys", async (req, res) => {
       const toyDetails = req.body;
-      //   const result = await toyCollection.insertOne();
+      const result = await toyCollection.insertOne(toyDetails);
       console.log(toyDetails);
+      res.send(result);
+    });
+
+    // get all toys from db
+    app.get("/toys", async (req, res) => {
+      const allToys = await toyCollection.find().toArray();
+      res.send(allToys);
+    });
+
+    // get single toy by id
+    app.get("/toy-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const toyDetails = await toyCollection.find(query).toArray();
       res.send(toyDetails);
+    });
+
+    // get all toys by seller
+    app.get("/my-toys/:uid", async (req, res) => {
+      const uid = req.params?.uid;
+      const query = { sellerUid: uid };
+      const toyBySeller = await toyCollection.find(query).toArray();
+      res.send(toyBySeller);
     });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
