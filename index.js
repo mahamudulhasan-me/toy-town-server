61988da9fa6f40e449d18be6b5b93c288604f997
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-24 13:57:09 +0600

    solve vercel issu

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
     //---------------------------------------------------
     // create database
     const toyTownDB = client.db("toyTownDB");
